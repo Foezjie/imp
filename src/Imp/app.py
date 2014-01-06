fced3e4ae17bc2f209c1a42528facaf68056256f
Bart Vanbrabant <bart@vanbrabant.eu>
2014-01-06 14:39:01 +0100

    Add option to only store the json output of the export command

--- src/Imp/app.py
+++ src/Imp/app.py
@@ -37,40 +37,40 @@ def do_compile(options, config):
         Run run run
     """
     libs = config.get("config", "lib-dir").split(":")
-    
+
     console = logging.StreamHandler()
     LOGGER.addHandler(console)
-    
+
     if options.debug:
         LOGGER.setLevel(logging.DEBUG)
     else:
         LOGGER.setLevel(logging.INFO)
 
     compiler = Compiler(config, libs)
-    
+
     success = False
     try:
         graph = compiler.graph
         statements = compiler.compile()
         sched = scheduler.Scheduler(graph)
         success = sched.run(compiler, statements)
-        
+
         if not success:
             sys.stderr.write("Unable to execute all statements.\n")
         else:
             return graph.root_scope
-        
+
     finally:
         pass
-    
+
 @command("server", help = "Start the imp server")
 def start_server(**kwargs):
     config = kwargs["config"]
     server = ImpServer(config)
     server.run()
-    
+
 @command("agent", help = "Start the imp agent", arguments = \
-         ( 
+         (
           ("-s", "Run the agent as a simulator", "store_true", "simulate"),
           ("--host", "Set the hostname of this agent", "hostname"))
          )
@@ -81,10 +81,10 @@ def start_agent(**kwargs):
     hostnames = []
     if options.hostname is not None:
         hostnames.extend(options.hostname)
-        
+
     elif config.has_section("agent") and "hostnames" in config["agent"]:
         hostnames.extend([x.strip() for x in config["agent"]["hostnames"].split(",")])
-    
+
     agent = Agent(config, options.simulate, hostnames)
     agent.run()
 
@@ -92,58 +92,59 @@ def start_agent(**kwargs):
 def compile_project(**kwargs):
     options = kwargs["options"]
     config = kwargs["config"]
-    
+
     if options.profile:
         import cProfile
         import pstats
-        result = cProfile.runctx('do_compile(options, config)', globals(), 
+        result = cProfile.runctx('do_compile(options, config)', globals(),
             { 'options': options, 'config' : config }, "run.profile")
         p = pstats.Stats('run.profile')
         p.strip_dirs().sort_stats("time").print_stats(20)
     else:
         result = do_compile(options, config)
-        
+
     return result
-      
-@command("list-commands", help = "Print out an overview of all commands")  
+
+@command("list-commands", help = "Print out an overview of all commands")
 def list_commands(**kwargs):
     print("The following commands are available:")
     for cmd in Commander.get_commands():
         print(" %s: %s" % (cmd, Commander.get_help(cmd)))
-        
-@command("list-modules", 
+
+@command("list-modules",
     help = "Print out a list of all modules available in this project and the location of the module")
 def list_modules(**kwargs):
     pass
     # TODO
 
 @command("export", help = "Export the configuration", requires = ["compile"],
-         arguments = (("-g", "Dump the dependency graph", "store_true", "depgraph"),))
+         arguments = (("-g", "Dump the dependency graph", "store_true", "depgraph"),
+                      ("-j", "Do not submit to the server but only store the json that would have been submitted in the supplied file", "json")))
 def export(options, config, compile):
     from Imp.export import Exporter
     export = Exporter(config, options)
     export.run(compile)
 
-@command("deploy", help = "Deploy the configuration model on the current machine", 
+@command("deploy", help = "Deploy the configuration model on the current machine",
         requires = ["compile"],
         arguments = (("-r", "Deploy on this remote host", "remote"),
                      ("-i", "The ip to connecto to", "ip"),
-                     ("--dry-run", "Only report changes", "store_true", "dryrun")))    
+                     ("--dry-run", "Only report changes", "store_true", "dryrun")))
 def deploy(options, config, compile):
     from Imp.deploy import deploy
     deploy(config, compile, remote = options.remote, dry_run = options.dryrun, ip_address=options.ip)
-    
+
 @command("client", help = "A client to send commands to IMP agents", arguments = \
          (("cmd", "The command to run"),))
 def client(options, config):
     from  Imp.server.client import Client
-    
+
     client = Client(config)
     client.execute(options.cmd, options.other)
 
 @command("hook", help = "A hook to compile the project")
 def hook(options, config):
-    
+
     pass
 
 @command("bootstrap", help = "Bootstrap the infrastructure")
@@ -164,32 +165,32 @@ def app():
     parser.add_argument("-d", "--debug", dest = "debug", action = "store_true",
                         help="Enable debug logging")
     parser.add_argument("-c", "--config", dest = "config_file", help="Use this config file")
-    
+
     subcommands = parser.add_subparsers()
-    
+
     for cmd in Commander.get_commands():
         sub_cmd = subcommands.add_parser(cmd)
         sub_cmd.set_defaults(command = cmd)
-        
+
         arguments = Commander.get_arguments(cmd)
         for argument in arguments:
             if len(argument) == 3:
-                sub_cmd.add_argument(argument[0], help = argument[1], 
+                sub_cmd.add_argument(argument[0], help = argument[1],
                                      dest = argument[2])
             elif len(argument) == 2:
                 sub_cmd.add_argument(argument[0], help = argument[1])
             else:
-                sub_cmd.add_argument(argument[0], help = argument[1], 
+                sub_cmd.add_argument(argument[0], help = argument[1],
                                      dest = argument[3], action = argument[2])
-                
-    
+
+
     options, other = parser.parse_known_args()
     options.other = other
-    
+
     if options.debug:
         # TODO: fix logging
         logging.basicConfig(level=logging.DEBUG)
-    
+
     if options.debugger:
         try:
             import pydevd; pydevd.settrace()
@@ -198,12 +199,12 @@ def app():
 
     Config.load_config(options.config_file)
     config = Config.get()
-    
+
     if not hasattr(options, "command"):
         # show help
         parser.print_usage()
         return
-    
+
     Commander.run(options.command, options, config)
 
 if __name__ == "__main__":
